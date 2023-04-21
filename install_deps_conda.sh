1cfb9af45a40306970ebd6046c6e36280584e92e
Eric Berquist <eric.berquist@gmail.com>
2023-04-21 11:46:27 -0400

    fix Python activation?

--- install_deps_conda.sh
+++ install_deps_conda.sh
@@ -2,6 +2,7 @@
 
 set -eu
 
+"${PYENV_ROOT}"/bin/pyenv init
 "${PYENV_ROOT}"/bin/pyenv shell miniforge3-22.11.1-4
 # python -m pip install -U pip setuptools
 # python -m pip config list
--- install_deps_venv.sh
+++ install_deps_venv.sh
@@ -3,6 +3,7 @@
 set -eu
 
 "${PYENV_ROOT}"/bin/pyenv install -s 3."${PYTHON_MINOR_VERSION}"
+"${PYENV_ROOT}"/bin/pyenv init
 "${PYENV_ROOT}"/bin/pyenv shell 3."${PYTHON_MINOR_VERSION}"
 python -m pip install -U pip setuptools
 python -m pip config list
