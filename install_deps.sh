2b0980edfcfc98debbf980171a87c6abdafd923c
Eric Berquist <eric.berquist@gmail.com>
2023-04-21 12:35:34 -0400

    dummy provenance for conda base

--- install_deps.sh
+++ install_deps.sh
@@ -19,10 +19,17 @@ if [ "${PYTHON_ENV_TYPE}" != "${PYTHON_ENV_TYPE_CONDA}" ] \
     exit 1
 fi
 
+# Install the desired Python version using pyenv.  If the environment is
+# supposed to be conda-based, then it must have already been installed via
+# some other mechanism..
 if [ "${PYTHON_ENV_TYPE}" = "${PYTHON_ENV_TYPE_VENV}" ]; then
     PYENV_VERSION=3."${PYTHON_MINOR_VERSION}"
     "${PYENV_ROOT}"/bin/pyenv install -s "${PYENV_VERSION}"
 elif [ "${PYTHON_ENV_TYPE}" = "${PYTHON_ENV_TYPE_CONDA}" ]; then
+    # This is the expected install mechanism.  You could have installed it via
+    # another nefarious way, but this provides a sort of provenance to show
+    # what "base" conda version you might expect.
+    [ -f ./install_base_conda.sh ] || exit 1
     PYENV_VERSION="${PYENV_CONDA_BASE}"
 fi
 export PYENV_VERSION
