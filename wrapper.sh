d461a0be5654f5edf6f8aea6a56dfbd79cc5df38
Eric Berquist <eric.berquist@gmail.com>
2023-04-26 11:20:07 -0400

    start making subdirectories

--- wrapper.sh
+++ wrapper.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# A simple wrapper that handles installing and testing the packages of
+# interest in the correct order.
+
 set -eux
 
 PYTHON_ENV_TYPE="${1}"
@@ -7,7 +10,8 @@ PYTHON_MINOR_VERSION="${2}"
 PACKAGE_BASE_PATH="${3}"
 
 logname="log-${PYTHON_ENV_TYPE}-${PYTHON_MINOR_VERSION}.txt"
-rm "${logname}"
+rm "${logname}" || tmp=$?
+# order matters!
 for package in libstore libjournal libarchive; do
     ./install_deps_and_test.sh "${PYTHON_ENV_TYPE}" "${PYTHON_MINOR_VERSION}" "${PACKAGE_BASE_PATH}/${package}" | tee -a "${logname}"
 done
