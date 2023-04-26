62b22d6bda7f3344f5a97755c1f134ce227beb19
Eric Berquist <eric.berquist@gmail.com>
2023-04-26 11:53:29 -0400

    disable pytest colors in CI, don't test libarchive

--- wrapper.sh
+++ wrapper.sh
@@ -12,6 +12,7 @@ PACKAGE_BASE_PATH="${3}"
 logname="log-${PYTHON_ENV_TYPE}-${PYTHON_MINOR_VERSION}.txt"
 rm "${logname}" || tmp=$?
 # order matters!
-for package in libstore libjournal libarchive; do
+# libarchive has been removed as long as it is broken
+for package in libstore libjournal; do
     ./install_deps_and_test.sh "${PYTHON_ENV_TYPE}" "${PYTHON_MINOR_VERSION}" "${PACKAGE_BASE_PATH}/${package}" | tee -a "${logname}"
 done
