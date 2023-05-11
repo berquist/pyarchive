5dc9fc5998be05915ea34c450bb01fb9066bf0cf
Eric Berquist <eric.berquist@gmail.com>
2023-05-11 16:37:56 -0400

    attempt build isolation via separating wheel creation from installation

--- install_deps_and_test.bash
+++ install_deps_and_test.bash
@@ -81,9 +81,13 @@ install_and_test_package() {
     local package_name
     package_name=$(basename "${_PACKAGE_PATH}")
 
-    # Finally install the package.  Don't clean the build directory in order
-    # to stop a concurrent process from trying to remove it.
-    python -m pip install --no-clean "${_PACKAGE_PATH}"
+    # Finally install the package.  Avoid concurrent process race conditions
+    # by not using a generic (wheel) build location.
+    python -m pip wheel \
+           --build-option --bdist-dir "${_PACKAGE_PATH}/build_${PYTHON_ENV_TYPE}_${python_version}" \
+           -w "${_PACKAGE_PATH}/wheels" \
+           "${_PACKAGE_PATH}"
+    python -m pip install "${_PACKAGE_PATH}/wheels"
     local PACKAGE_INSTALL_DIR
     PACKAGE_INSTALL_DIR=$(python -c "import ${package_name} as _; print(_.__path__[0])")
     find "${PACKAGE_INSTALL_DIR}" -type f | sort
