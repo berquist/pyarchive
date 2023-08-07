4963e5f97833b7103672d3e6864a12ea0f68bf00
Eric Berquist <eric.berquist@gmail.com>
2023-08-07 18:18:27 -0400

    translate validation output to script exit code

--- validate.sh
+++ validate.sh
@@ -4,5 +4,6 @@ set -euo pipefail
 
 JENKINS_URL="http://localhost:8090"
 JENKINS_CRUMB=$(curl "${JENKINS_URL}/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,\":\",//crumb)")
-# echo "${JENKINS_CRUMB}"
-curl -X POST -H "${JENKINS_CRUMB}" -F "jenkinsfile=<Jenkinsfile" "${JENKINS_URL}/pipeline-model-converter/validate"
+JENKINS_VALIDATE_OUTPUT=$(curl -X POST -H "${JENKINS_CRUMB}" -F "jenkinsfile=<Jenkinsfile" "${JENKINS_URL}/pipeline-model-converter/validate")
+echo "${JENKINS_VALIDATE_OUTPUT}"
+[[ "${JENKINS_VALIDATE_OUTPUT}" =~ "successfully validated" ]]
